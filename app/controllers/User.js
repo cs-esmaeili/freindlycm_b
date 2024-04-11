0e6fe5bef39db61dfc4e71a226746d05befef417
Javad Esmaeili <cs.esmaeili@gmail.com>
2024-04-11 16:50:41 +0330

    Delete user added

--- app/controllers/User.js
+++ app/controllers/User.js
@@ -1,5 +1,6 @@
 const User = require('../database/models/User');
-const { mAddUser } = require('../messages/response.json');
+const Hero = require('../database/models/Hero');
+const { mAddUser, mDeleteUser } = require('../messages/response.json');
 
 exports.userList = async (req, res) => {
     const list = await User.find({}).populate({ path: 'heroList', populate: { path: 'class' } }).lean();
@@ -11,13 +12,29 @@ exports.userList = async (req, res) => {
 exports.addUser = async (req, res) => {
     try {
         const { name, leader } = req.body;
-        const result = User.create({ name, leader });
+        const result = User.create({ name, leader, heroList: [] });
         if (result) {
             res.send({ message: mAddUser.ok });
             return;
         }
         throw { message: mAddUser.fail, statusCode: 401 };
     } catch (err) {
+        res.status(err.statusCode || 422).json(err);
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const { user_id } = req.body;
+        const userResult = await User.deleteOne({ _id: user_id });
+        const heroResult = await Hero.deleteMany({ user: user_id });
+
+        if (userResult.deletedCount == 0 ) {
+            throw { message: mDeleteUser.fail, statusCode: 500 };
+        }
+        res.send({ message: mDeleteUser.ok });
+    } catch (err) {
+        console.log(err);
         res.status(err.statusCode || 422).json(err);
     }
 }
